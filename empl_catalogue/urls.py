98203b0cf77563d6aa8ee8c5b2304758b5f3c9c5
triple-s-rank <specialforwar@icloud.com>
2022-07-30 17:02:46 +0300

    Add collapse employees to template

--- empl_catalogue/urls.py
+++ empl_catalogue/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalogue/', include('catalogue.urls')),
-    path("__reload__/", include("django_browser_reload.urls")),
 ]
